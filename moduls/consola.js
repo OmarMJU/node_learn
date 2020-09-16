8fd769121aa23fc83e54c922f327afa79359830b
Omar M Juarez <omarmju906p@hotmail.com>
2020-09-16 17:15:15 -0500

    Se agrega ejemplos del módulo de CONSOLE.

---
+++ moduls/consola.js
@@ -0,0 +1,17 @@
+const miArray = [
+    { nombre: "Omar", edad:29 },
+    { nombre: "Juarez", edad:30 },
+    { nombre: "Danny", edad:31 },
+    { nombre: "Zamudio", edad:32 }
+];
+
+const misNumeros = [1, 2, 3, 4, 5];
+
+
+console.log("La consola que todos conocemos");
+console.info("Todo esto paracera en el log como información pero no en archivos de salida");
+console.warn("Esto es un warning");
+console.error("Esto es un espantoso error.");
+console.table(miArray);
+console.log(miArray);
+console.table(misNumeros);
