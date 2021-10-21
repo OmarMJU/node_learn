9bae35cf00bd0d85ce928dafdce8119e03a47d26
Omar M Juarez <omarmju906p@hotmail.com>
2021-10-20 22:04:29 -0500

    Se agregan más ejemplos de Benchmarking.

--- trucos/benchmarking.js
+++ trucos/benchmarking.js
@@ -1,4 +1,7 @@
-console.time("Programa")
+// Comienza a tomar el tiempo para todo lo que esta entre la etiqueta "Programa".
+console.time("Programa");
+
+
 function suma (a, b) {
     return a + b;
 }
@@ -6,9 +9,46 @@ function suma (a, b) {
 const num1 = 21;
 const num2= 32;
 
+// Conteo de tiempo para "Suma": Inicio.
 console.time("Suma");
-console.log("Iniciando Suma");
 suma(num1, num2);
-console.log("Finalizando Suma");
+// Conteo de tiempo para "Suma": Fin.
 console.timeEnd("Suma");
+
+
+// #######################
+// ### PARA CICLOS FOR ###
+// #######################
+
+
+console.time("Ciclo");
+
+for (let i = 0; i < 100000000; i++) {
+    suma(i, i + 1);
+}
+console.timeEnd("Ciclo");
+
+
+// #################################
+// ### PARA FUNCIONES ASINCRONAS ###
+// #################################
+
+console.time("Asincrona");
+
+async function asyncFunction() {
+    return await new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Dentro de la funcion asincrona")
+            resolve();
+        }, 2000);
+    });
+}
+
+asyncFunction().then(() => {
+    console.timeEnd("Asincrona");
+});
+
+
+
+// Finaliza el contador de tiempo para la etiqueta "Programa".
 console.timeEnd("Programa");
